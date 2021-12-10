36faec2fd57bc722c396d2a2c896a45219414e40
BigArtemka <dak.112008@gmail.com>
2021-12-11 02:51:01 +0300

    Initial commit

---
+++ src/main/java/ru/filimonov/kanyequotecatpicture/KanyeQuoteCatPictureApplication.java
@@ -0,0 +1,13 @@
+package ru.filimonov.kanyequotecatpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanyeQuoteCatPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanyeQuoteCatPictureApplication.class, args);
+    }
+
+}

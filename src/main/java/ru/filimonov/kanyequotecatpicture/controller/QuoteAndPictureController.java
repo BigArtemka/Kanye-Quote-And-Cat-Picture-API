b4e137025e6fa4b13559ea98bb8e7c63df649eed
BigArtemka <dak.112008@gmail.com>
2021-12-11 05:18:14 +0300

    #5 - Формат тела ответа изменен с json на html, сгенерированный thymeleaf'ом

--- src/main/java/ru/filimonov/kanyequotecatpicture/controller/QuoteAndPictureController.java
+++ src/main/java/ru/filimonov/kanyequotecatpicture/controller/QuoteAndPictureController.java
@@ -1,22 +1,22 @@
 package ru.filimonov.kanyequotecatpicture.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.filimonov.kanyequotecatpicture.model.QuoteAndPicture;
 import ru.filimonov.kanyequotecatpicture.service.QuoteAndPictureService;
 
 @Validated
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class QuoteAndPictureController {
     final QuoteAndPictureService service;
 
     @GetMapping("/*")
-    public ResponseEntity<QuoteAndPicture> getQuoteAndPicture() {
-        return ResponseEntity.ok(service.getQuoteAndPicture());
+    public String getQuoteAndPicture(Model model) {
+        model.addAttribute("quoteandpicture", service.getQuoteAndPicture());
+        return "quote-and-picture";
     }
 
 }
